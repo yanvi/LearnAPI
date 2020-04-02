edc41b24ae9a354b5fc191265ae72d15dea27c1f
vinay <vinayp771@gmail.com>
2020-04-03 00:17:09 +0530

    post method created

--- PracticeAPI/Controllers/ProductController.cs
+++ PracticeAPI/Controllers/ProductController.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using PracticeAPI.Models;
 
 namespace PracticeAPI.Controllers
 {
     public class ProductController : ApiController
     {
-        
+
+        [HttpPost]
+        public HttpResponseMessage Post(Product product)
+        {
+            try
+            {
+
+                ProductDB obj = new ProductDB();
+               int x = obj.Save(product);
+                
+                    var message = Request.CreateResponse(HttpStatusCode.Created, product);
+                    message.Headers.Location = new Uri(Request.RequestUri +
+                        product.Id.ToString());
+               
+                   
+
+                    return message;
+                
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
     }
 }
